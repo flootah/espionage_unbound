44db1804ecebc8d3ba84669d4ac4028a39b934ea
Lance Dall <lmdall@cpp.edu>
2017-05-11 21:33:44 -0700

    Made a board with 9x9 dimensions.  Declared three enum types; P for Player, A for Assassin, B for Briefcase.  This commit+push is primarily a test to see if you can pull without any problems!

--- src/edu/cpp/cs/cs141/prog_final/GameBoard.java
+++ src/edu/cpp/cs/cs141/prog_final/GameBoard.java
@@ -1,5 +1,20 @@
 package edu.cpp.cs.cs141.prog_final;
 
 public class GameBoard {
+    
+    public static enum BoardMark {P, A, B}
+    
+    public static final int BOARD_SIZE = 9;
+    /**
+     * Method that creates the board.
+     */
+    public void createBoard() {
+        String gameGrid[][] = new String[BOARD_SIZE][BOARD_SIZE];
+        for (int i = 0; i < gameGrid.length; i++) {
+            for (int j = 0;j < gameGrid.length; j++) {
+                System.out.println(gameGrid[i][j]);
+            }
+        }
+    }
 
 }
