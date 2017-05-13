be3c9d04a63121ce2d12d65908930af0c4759de6
Corey Perez <coreymperez@cpp.edu>
2017-05-13 04:25:04 +0000

    Gun.java filled out.

--- src/edu/cpp/cs/cs141/prog_final/Gun.java
+++ src/edu/cpp/cs/cs141/prog_final/Gun.java
@@ -9,4 +9,29 @@ package edu.cpp.cs.cs141.prog_final;
  */
 public class Gun{
 
+    private int ammo;
+	
+    private final int maxAmmo = 1;
+	
+	public Gun()
+	{
+		ammo = maxAmmo;
+	}
+
+	
+	public void shoot()
+	{
+		ammo--;
+	}
+	
+	public void reload()
+	{
+		ammo = maxAmmo;
+	}
+	
+	public int getAmmo()
+	{
+		return ammo;
+	}
+
 }
