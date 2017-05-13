330b0893196807c2cd4771c437cc079233a634a4
Corey Perez <coreymperez@cpp.edu>
2017-05-13 04:26:09 +0000

    Player.java filled out

--- src/edu/cpp/cs/cs141/prog_final/Player.java
+++ src/edu/cpp/cs/cs141/prog_final/Player.java
@@ -9,4 +9,31 @@ package edu.cpp.cs.cs141.prog_final;
  */
 public class Player extends ActiveAgents {
 
+    private final int maxLives = 3;
+	
+	private int lives;
+	
+	private Gun gun;
+	
+	public Player(Gun weapon)
+	{
+		gun = weapon;
+		lives = maxLives;
+	}
+	
+	public void shoot()
+	{
+		gun.shoot();
+	}
+	
+	public void reloadPlayerGun()
+	{
+		gun.reload();
+	}
+	
+	public int getLives()
+	{
+		return lives;
+	}
+
 }
