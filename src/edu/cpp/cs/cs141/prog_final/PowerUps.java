39238a60397aaadb52ff61251bcec42ed9451475
Corey Perez <coreymperez@cpp.edu>
2017-05-19 23:58:46 +0000

    PowerUps.java edited online with Bitbucket

--- src/edu/cpp/cs/cs141/prog_final/PowerUps.java
+++ src/edu/cpp/cs/cs141/prog_final/PowerUps.java
@@ -9,4 +9,16 @@ package edu.cpp.cs.cs141.prog_final;
  */
 public abstract class PowerUps {
 
+    public int calculateRow()
+	{
+		int randRow = new Random().nextInt(9);
+		return randRow;
+	}
+	
+	public int calculateColumn()
+	{
+		int randColumn = new Random().nextInt(9);
+		return randColumn;
+	}
+
 }
