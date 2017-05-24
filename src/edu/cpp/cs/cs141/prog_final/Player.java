fbc28316e72fac53ba535c9940179519c502b786
Corey Perez <coreymperez@cpp.edu>
2017-05-24 04:47:09 +0000

    Player.java edited online with Bitbucket

--- src/edu/cpp/cs/cs141/prog_final/Player.java
+++ src/edu/cpp/cs/cs141/prog_final/Player.java
@@ -44,46 +44,6 @@ public class Player extends ActiveAgents {
 		return lives;
 	}
 	
-	public void move(char userMove)
-	{
-		switch (userMove) {
-		case 'W':
-		case 'w':
-			if (row > 0) {
-		    row--;
-			} else {
-			System.out.println("It's a wall.");
-			}
-		    break;
-		case 'S':
-		case 's':
-		    if (row < 8) {
-		    row++;
-			} else {
-			System.out.println("It's a wall.");
-			}
-		    break;
-		case 'A':
-		case 'a':
-		    if (column > 0) {
-		    column--;
-			} else {
-			System.out.println("It's a wall.");
-			}
-		    break;
-		case 'D':
-		case 'd':
-		    if (column < 8) {
-		    column++;
-			} else {
-			System.out.println("It's a wall.");
-			}
-		    break;
-		default:
-		    System.out.println("Invalid move selection");
-		}
-	}
-	
 	public int getRow()
 	{
 		return row;
