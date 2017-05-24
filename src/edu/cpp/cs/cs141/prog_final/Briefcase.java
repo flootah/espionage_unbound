4bbbb1310ab60d8b1cf31e65937846737dea9e4f
Corey Perez <coreymperez@cpp.edu>
2017-05-24 02:31:37 +0000

    Briefcase.java edited online with Bitbucket

--- src/edu/cpp/cs/cs141/prog_final/Briefcase.java
+++ src/edu/cpp/cs/cs141/prog_final/Briefcase.java
@@ -10,10 +10,34 @@ package edu.cpp.cs.cs141.prog_final;
 
 public class Briefcase
 {
+    private int row;
+    
+    private int column;
+
     private String briefCaseMark = "B";
 	
 	public String getBriefCaseMark()
 	{
 		return briefCaseMark;
+	}
+    
+    public void setRow(int r)
+	{
+		row = r;
+	}
+	
+	public void setColumn(int c)
+	{
+		column = c;
+	}
+
+	public int getRow() 
+	{
+		return row;
+	}
+
+	public int getColumn()
+	{
+		return column;
 	}
 }
